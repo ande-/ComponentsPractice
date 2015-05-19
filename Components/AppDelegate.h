4180fe4c7127d52f982538869d895ae0e8f1f637
cog1 <andrea.houg@gmail.com>
2015-05-19 16:36:50 -0400

    Initial Commit

---
+++ Components/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Components
+//
+//  Created by Andrea Houg on 5/19/15.
+//  Copyright (c) 2015 a. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Components/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Components
+//
+//  Created by Andrea Houg on 5/19/15.
+//  Copyright (c) 2015 a. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
